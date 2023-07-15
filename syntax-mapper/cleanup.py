90906f42d5acc362255ac87a29ad41139cbe5bcb
Paul Tarvydas <guitarvydas@gmail.com>
2023-07-15 06:56:10 -0400

    rm proc replace by procSignature - not working yet

--- syntax-mapper/cleanup.py
+++ syntax-mapper/cleanup.py
@@ -3,5 +3,5 @@
 import sys
 
 for line in sys.stdin:
-    r = line.replace ('・', ' ').replace ('⦚', '\n').replace ('◦', ' ').replace ('¶', '\n').replace ('□', ' ').replace ('‡', '\n').replace ('☐', ' ').replace ('†', '\n') ##.replace ('⎧', '').replace ('⎭', '')
+    r = line.replace ('・', ' ').replace ('⦚', '\n').replace ('◦', ' ').replace ('¶', '\n').replace ('□', ' ').replace ('‡', '\n').replace ('☐', ' ').replace ('†', '\n').replace ('⎧', '').replace ('⎭', '')❫.replace ('❪', '(').replace ('❫', ')')
     print (r, end='')
