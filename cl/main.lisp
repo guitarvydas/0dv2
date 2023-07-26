7f7d31e865d3cca59de3237f58ddc5c44ab5b54d
Paul Tarvydas <guitarvydas@gmail.com>
2023-07-26 05:02:34 -0400

    beta main.lisp

---
+++ cl/main.lisp
@@ -0,0 +1,28 @@
+(defun echo-handler (eh msg)
+  (send eh "stdout" (slot-value msg 'datum)))
+
+(defun main ()
+  (format *standard-output* "*** Handmade Visibility Jam ***")
+  (format *standard-output* "--- Super Basic: Single ---")
+  (let ((echo0 (Leaf/New "10" #'echo-handler))
+	(top (Container/new "Top")))
+    (setf (slot-value top 'children)
+	  (list echo0))
+    (let ((child0 (nth 0 (slot-value top 'children))))
+      (setf (slot-value top 'connections)
+	    (list
+	     (Connector/new 'Down 
+			     (Sender/new 'self "stdin")
+			     (Receiver/new (slot-value child0 'input) "stdin"))
+	     (Connector/new 'Up
+			     (Sender/new child0 "stdout")
+			     (Receiver/new 'self "stdout"))
+	     )
+	    )
+      )
+    (funcall (slot-value top 'handler)
+             top
+             (Message/new "stdin" "hello"))
+    (slot-value top 'output)))
+	  
+	  
