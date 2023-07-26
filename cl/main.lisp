0ac5bfe2de12642454fd6f773f454deba1a1f979
Paul Tarvydas <guitarvydas@gmail.com>
2023-07-26 07:11:27 -0400

    parallel working with debugs

--- cl/main.lisp
+++ cl/main.lisp
@@ -52,6 +52,36 @@
              (Message/new "stdin" "hello"))
     (format *standard-output* "~%output: ~a" (as-list (slot-value top 'output)))
     )
+  (format *standard-output* "~%--- Basic: Parallel ---")
+  (let ((echo0 (Leaf/New "10" #'echo-handler))
+        (echo1 (Leaf/New "11" #'echo-handler))
+	(top (Container/new "Top")))
+    (setf (slot-value top 'children)
+	  (list echo0 echo1))
+    (let ((child0 (nth 0 (slot-value top 'children)))
+          (child1 (nth 1 (slot-value top 'children))))
+      (setf (slot-value top 'connections)
+	    (list
+	     (Connector/new 'Down 
+                            (Sender/new top "stdin")
+                            (Receiver/new (slot-value child0 'input) "stdin"))
+	     (Connector/new 'Down 
+                            (Sender/new top "stdin")
+                            (Receiver/new (slot-value child1 'input) "stdin"))
+	     (Connector/new 'Up
+                            (Sender/new child0 "stdout")
+                            (Receiver/new (slot-value top 'output) "stdout"))
+	     (Connector/new 'Up
+                            (Sender/new child1 "stdout")
+                            (Receiver/new (slot-value top 'output) "stdout"))
+	     )
+	    )
+      )
+    (funcall (slot-value top 'handler)
+             top
+             (Message/new "stdin" "hello"))
+    (format *standard-output* "~%output: ~a" (as-list (slot-value top 'output)))
+    )
   )
 	  
 	  
